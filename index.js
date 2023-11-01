de25789cd2db4de81adc75c4875564af46acde0f
srs-sieam <srssieam@gmail.com>
2023-11-01 18:56:53 +0600

    get total data count from database

--- index.js
+++ index.js
@@ -35,6 +35,11 @@ async function run() {
         res.send(result);
     })
 
+    app.get('/productsCount', async (req, res)=>{
+      const count = await productCollection.estimatedDocumentCount();
+      res.send({count});
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
